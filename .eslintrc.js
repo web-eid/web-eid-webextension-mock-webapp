5ec7ea566bf3181760d8f3ab834783c8a54d01ab
Tanel Metsar <tanel.metsar@cgi.com>
2021-12-01 19:35:45 +0200

    feat: JavaScript API CORS and CSP related vulnerability fixes (option A)
    * Use library version 2.0.0
    * Update options on the Web-eID page to reflect library API changes
    * Separate headers option for each request
    * Added ESLint import ordering rule and reordered the imports
    
    Signed-off-by: Tanel Metsar <tanel.metsar@cgi.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,36 @@
 module.exports = {
   root: true,
+
   env: {
     browser: true,
-    node: true,
-    es6: true,
+    node:    true,
+    es6:     true,
   },
+
   extends: [
-    "eslint:recommended"
+    "eslint:recommended",
   ],
+
   rules: {
-    "quotes": ["error", "double", { "avoidEscape": true }],
-    "semi": "off",
-    "indent": ["error", 2],
-    "brace-style": "error",
-    "key-spacing": ["error", { "align": "value" }],
+    "quotes":                ["error", "double", { "avoidEscape": true }],
+    "semi":                  "off",
+    "indent":                ["error", 2],
+    "brace-style":           "error",
+    "key-spacing":           ["error", { "align": "value" }],
+    "object-curly-spacing":  ["error", "always"],
+    "array-bracket-spacing": "error",
+    "sort-imports":          ["error", { allowSeparatedGroups: true }],
+
     "comma-dangle": ["error", {
-      "arrays": "always-multiline",
+      "arrays":  "always-multiline",
       "objects": "always-multiline",
       "imports": "always-multiline",
       "exports": "always-multiline",
     }],
-    "object-curly-spacing": ["error", "always"],
-    "array-bracket-spacing": "error",
   },
+
   parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2018
-  }
+    sourceType:  "module",
+    ecmaVersion: 2018,
+  },
 };
